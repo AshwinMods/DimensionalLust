6b79ba96251e2713e855f6ccfc7c3b28464ecd97
AshwinMods <ashwin.mods@gmail.com>
2023-07-22 20:27:06 +0530

    Oh! Scarp

--- Assets/Scipts/DimensionConfig.cs
+++ Assets/Scipts/DimensionConfig.cs
@@ -6,16 +6,129 @@ using UnityEngine;
 [System.Serializable]
 public class DimensionConfig : ScriptableObject
 {
+	[System.Serializable]
+	public class Noise
+	{
+		public string m_Name;
+		public Vector4[] m_TileOffset;
+		public Vector4[] m_MulMinMaxAdd;
+		public float Evaluate(float x, float y) 
+		{
+			Vector4 vec;
+			float res = 0, val;
+			for (int i = 0, iMax = m_TileOffset.Length; i < iMax; i++)
+			{
+				vec = m_TileOffset[i];
+				val = Mathf.PerlinNoise(x * vec.x + vec.z, y * vec.y + vec.w);
+
+				vec = m_MulMinMaxAdd[i];
+				val *= vec.x;
+				val = Mathf.Clamp(val, vec.y, vec.z);
+				// Is Additive or Mul
+				if(vec.w == 0) res += val;
+				else res += val * vec.w;
+			}
+			return res;
+		}
+	}
+	
+	[System.Serializable]
+	public class AssetRef
+	{
+		public string m_Name;
+		public Transform[] m_Variants;
+		public Transform GetRandomRef() => m_Variants.Length == 0 ? null : m_Variants[Random.Range(0, m_Variants.Length)];
+	}
+
+	[System.Serializable]
+	public class Placement
+	{
+		public string m_Name;
+		[Space]
+		public int m_AssetNoise;
+		[Space]
+		public int m_HeightNoise;
+		public float m_HeightFactor;
+		[Space]
+		public AssetRef[] m_Assets;
+	}
+
+	[System.Serializable]
+	public struct Tile
+	{
+		public Vector3 m_Height;
+		public Vector2Int m_GroundIndex;
+		public Vector2Int m_AssetIndex;
+	}
+
+	[System.Serializable]
+	public class WorldChunk
+	{
+		public Bounds m_Bounds;
+		public Transform m_Container;
+		public Tile[] m_Tiles;
+
+		public WorldChunk(Bounds a_Bounds)
+		{
+			m_Bounds = a_Bounds;
+			m_Container = new GameObject("Chunk").transform;
+			m_Container.position = m_Bounds.min;
+			m_Tiles = new Tile[(int)(m_Bounds.size.x * m_Bounds.size.z)];
+		}
+	}
+
+	public Noise[] m_Noise;
+	public Placement[] m_Placements;
+	[Space]
 	public Vector4 m_TileConfig;
 	public Transform[] m_PlatformPrefabs;
 
-	public void Generate(Transform m_Container, Vector4 a_WorldRange)
+	public WorldChunk Generate(Bounds a_Bounds)
 	{
+		var l_WChunk = new WorldChunk(a_Bounds);
+		var l_Tiles = l_WChunk.m_Tiles;
+
+		int l_Idx;
+		float x, z, xMax, zMax, xPos, zPos;
+		for (z = 0, zMax = a_Bounds.size.z; z < zMax; z++)
+		{
+			zPos = a_Bounds.min.z + z;
+			for (x = 0, xMax = a_Bounds.size.x; x < xMax; x++)
+			{
+				xPos = a_Bounds.min.x + x;
+				l_Idx = (int)(z * zMax + x);
+				for (int w = 0; w < m_Placements.Length; w++)
+				{
+					var l_P = m_Placements[w];
+
+					float l_HeightNoise = m_Noise[l_P.m_HeightNoise].Evaluate(xPos, zPos);
+					l_Tiles[l_Idx].m_Height.y = l_HeightNoise;
+
+					float l_AssetNoise = m_Noise[l_P.m_AssetNoise].Evaluate(xPos, zPos);
+					var l_AssLen = l_P.m_Assets.Length;
+					if (l_AssLen > 0)
+					{
+						int l_AssetIndex = (int)Mathf.Clamp(l_AssetNoise * l_AssLen, 0, l_AssLen - 1);
+
+						var l_AssetRef = l_P.m_Assets[l_AssetIndex].GetRandomRef();
+						if (l_AssetRef)
+						{
+							var l_AssetInst = Instantiate(l_AssetRef, l_WChunk.m_Container);
+							l_AssetInst.position = new Vector3(xPos, l_HeightNoise * l_P.m_HeightFactor, zPos);
+							//l_Tiles[l_Idx].m_AssetIndex.x = l_AssetIndex; l_P.m_Assets;
+						}
+					}
+				}
+
+			}
+		}
+		return l_WChunk;
+		/*
 		// Ground
 		var m_Platform = m_PlatformPrefabs;
-		for (float z = a_WorldRange.x; z < a_WorldRange.y; z++)
+		for (float z = a_Bounds.min.z; z < a_Bounds.max.x; z++)
 		{
-			for (float x = a_WorldRange.z; x < a_WorldRange.w; x++)
+			for (float x = a_Bounds.min.x; x < a_Bounds.max.x; x++)
 			{
 				var l_U = x * m_TileConfig.x + m_TileConfig.z;
 				var l_V = z * m_TileConfig.y + m_TileConfig.w;
@@ -26,5 +139,6 @@ public class DimensionConfig : ScriptableObject
 				l_Tile.position = new Vector3(x, 0, z);
 			}
 		}
+		*/
 	}
 }
--- Assets/Scipts/Generator.cs
+++ Assets/Scipts/Generator.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using static DimensionConfig;
 
 public class Generator : MonoBehaviour
 {
@@ -12,14 +13,19 @@ public class Generator : MonoBehaviour
 	[Header("Editor")]
 	[SerializeField] int m_DimIndex;
 	[SerializeField] bool m_Generate;
+
+	[SerializeField] WorldChunk m_Chunk;
 	private void Update()
 	{
 		if (m_Generate)
 		{
 			m_Generate = false;
-			Clear(m_Container);
-			var l_WorldRange = new Vector4(0, m_WorldSize.x, 0, m_WorldSize.y);
-			m_Dimensions[m_DimIndex].Generate(m_Container, l_WorldRange);
+			if(m_Chunk != null && m_Chunk.m_Container) Clear(m_Chunk.m_Container);
+
+			Bounds l_ChunkBound = new Bounds();
+			l_ChunkBound.min = new Vector3(0, 0, 0);
+			l_ChunkBound.max = new Vector3(m_WorldSize.x, 0, m_WorldSize.y);
+			m_Chunk = m_Dimensions[m_DimIndex].Generate(l_ChunkBound);
 		}
 	}
 
